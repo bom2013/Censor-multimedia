9c52683826d55bbe7f593835575ac07e88a0fe0a
bom2013 <noam21288@gmail.com>
2020-07-09 19:23:01 +0300

    Add basic gui

---
+++ Censor multimedia/CensorPart.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Censor_multimedia
+{
+    partial class CensorPart
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLable = new System.Windows.Forms.Label();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timeLable
+            // 
+            this.timeLable.AutoSize = true;
+            this.timeLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLable.Location = new System.Drawing.Point(3, 18);
+            this.timeLable.Name = "timeLable";
+            this.timeLable.Size = new System.Drawing.Size(363, 46);
+            this.timeLable.TabIndex = 0;
+            this.timeLable.Text = "00:00:00 - 00:00:00";
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(361, 3);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(93, 78);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "X";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // CensorPart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.timeLable);
+            this.Name = "CensorPart";
+            this.Size = new System.Drawing.Size(457, 89);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLable;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
